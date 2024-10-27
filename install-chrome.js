b5700ec4dea29c4ba3c5946b9490a9963f18ab62
linsch9 <55497886+linsch9@users.noreply.github.com>
2024-10-27 22:33:30 +0100

    Update install-chrome.js

--- install-chrome.js
+++ install-chrome.js
@@ -1,17 +1,17 @@
-process.env.PUPPETEER_CACHE_DIR = '/opt/render/.cache/puppeteer';
-
 const puppeteer = require('puppeteer');
+const path = require('path');
 
-async function installChrome() {
+(async () => {
     try {
+        console.log('Setting puppeteer cache directory...');
+        process.env.PUPPETEER_CACHE_DIR = path.resolve(__dirname, '.cache/puppeteer');
+
         console.log('Downloading the Chromium browser...');
         await puppeteer.launch(); // Das wird den erforderlichen Chromium-Browser herunterladen
         console.log('Chromium browser downloaded successfully.');
-        process.exit(0); // Make sure to exit the process successfully
-    } catch (error) {
-        console.error('Error downloading the Chromium browser:', error);
-        process.exit(1); // Make sure to exit the process with error status
+        process.exit(0);
+    } catch (err) {
+        console.error('Error downloading the Chromium browser:', err);
+        process.exit(1);
     }
-}
-
-installChrome();
+})();
