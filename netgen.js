05307278a753974c71e86123a64d948804723433
linsch9 <55497886+linsch9@users.noreply.github.com>
2024-10-27 17:55:08 +0100

    Update netgen.js

--- netgen.js
+++ netgen.js
@@ -1,5 +1,3 @@
-// netgen.js
-
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
@@ -80,7 +78,7 @@ async function scrapeFollowings(userId, page, url, fullScroll = false, timeout =
 }
 
 async function getFullNetwork(discordUsername, soundcloudUsername, userId) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const mainPage = await browser.newPage();
     const userUrl = `https://soundcloud.com/${soundcloudUsername}/following`;
     const { mainProfile, followings: followings1 } = await scrapeFollowings(userId, mainPage, userUrl, true);
